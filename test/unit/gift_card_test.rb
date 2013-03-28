2d746b9faed6957d632839a984be6ec567187674
vandanpatel87 <vandanpatel87@gmail.com>
2013-03-28 14:54:42 -0500

    validations added

--- test/unit/gift_card_test.rb
+++ test/unit/gift_card_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GiftCardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "invalid without a sender" do
+		g = gift_cards(:preeti)
+		g.sender = nil
+		assert g.valid?
+		assert_match /can't be blank/, g.errors[:sender].join, "Presence error not found on gift card."
+	end
+
+	test "valid with all attributes" do
+		g = gift_cards(:vandan)
+		assert g.valid?, "GiftCard was not valid"
+	end
 end
