4057fd61758a00f6ec1378cc8f5c09ebf04d0d55
vandanpatel87 <vandanpatel87@gmail.com>
2013-03-28 13:35:24 -0500

    index.html deleted

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ GiftCardApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'gift_cards#index' 
 
   # See how all your routes lay out with "rake routes"
 
