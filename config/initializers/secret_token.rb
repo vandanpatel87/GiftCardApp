bede34d4b5b287a6d5666db9c412dfb6dfbc8458
vandanpatel87 <vandanpatel87@gmail.com>
2013-03-28 13:07:02 -0500

    first official commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftCardApp::Application.config.secret_token = '291ce057daaf07df7ce43cf4e45fc9f272a9d05472eb8a8accca53e36bdaae8f47be8e4cfccadd9239cd85b09548fab1ab01aaf89a2a2e638f83aa6bcd0358c8'
